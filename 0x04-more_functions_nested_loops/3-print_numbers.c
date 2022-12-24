4d72693c9df1dca43a99ce470d1ed510f152190d
DMDdotcom <diamond4mi@yahoo.com>
2022-12-24 01:20:10 -0800

    update gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-print_numbers.c -o 3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
-\**
- * print_numbers - check for a digit (0,1,2,3,4,5,6,7,8,9).
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-void print_number (void)
+void print_numbers(void)
 {
-	int c;
-
+int c;
 for (c = 48; c < 58; c++)
 {
 _putchar(c);
 }
-_putchar('/n');
+_putchar('\n');
 }
