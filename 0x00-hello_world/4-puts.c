7cae8ea6fa1c785ca08b07ce68738afd187f2c3a
DMDdotcom <diamond4mi@yahoo.com>
2022-12-24 20:32:53 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-
 /**
- * main - Entry point
+ * main -prints a giving phrase
  *
- * Return: Always 0 (success)
+ * Return: Always 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-        puts("\"Programming is like building a multi lingual puzzle);
+        puts("\"Programming is like building a multilingual puzzle);
         return (0);
 }
