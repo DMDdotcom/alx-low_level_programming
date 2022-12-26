1a55da5ce8d1f112e701e18f6aa193f68e99d6e9
DMDdotcom <diamond4mi@yahoo.com>
2022-12-25 18:23:11 -0800

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 
 printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu 4 byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long: %lu 8 byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long int: %lu 8 byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu 4 byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
