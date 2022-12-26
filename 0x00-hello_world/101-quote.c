b56bbb672a3637ef86cdf113e373329cb5bb9301
DMDdotcom <diamond4mi@yahoo.com>
2022-12-25 18:54:53 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
 * main - Entry
-* Return: Always 1
+* Return: Always 1 (success)
 */
 int main(void)
 {
-write("And that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput(1, "And that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
