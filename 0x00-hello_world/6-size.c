93bc17c4f949ea113233fc130f0f0fc37a96e603
DMDdotcom <diamond4mi@yahoo.com>
2022-12-25 17:56:26 -0800

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point (a C program that prints the size of various types on the computer)
+ * main - Entry point
  * Return: 0 (success)
  */
 int main(void)
