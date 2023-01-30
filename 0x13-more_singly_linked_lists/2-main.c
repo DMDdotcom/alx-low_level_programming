d9921281ee9e8eff39af2c42310e70f84a050e42
DMDdotcom <diamond4mi@yahoo.com>
2023-01-30 14:01:15 -0800

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/2-main.c
+++ 0x13-more_singly_linked_lists/2-main.c
@@ -24,13 +24,3 @@ int main(void)
     print_listint(head);
     return (0);
 }
-julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-add_nodeint.c 0-print_listint.c -o c
-julien@ubuntu:~/0x13. More singly linked lists$ ./c 
-1024
-402
-98
-4
-3
-2
-1
-0
